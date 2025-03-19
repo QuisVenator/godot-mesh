20a58d8a6c06886d20030ac911a1b9977e5bf1ba
René Pauls <paulsmrene@live.com>
2025-03-19 01:17:04 -0300

    Add creative movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = 7
 const GRAVITY = Vector3(0, -9.8, 0)
 
 var sensitivity = 0.002
+var creative_mode = true
 @onready var camera: Camera3D = $Camera3D
 @onready var raycast: RayCast3D = $Camera3D/RayCast3D
 @onready var coordinates: Label = $Camera3D/Label
@@ -26,13 +27,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotation.y = rotation.y - event.relative.x * sensitivity
 		camera.rotation.x = camera.rotation.x - event.relative.y * sensitivity
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(175))
-	elif event is InputEventKey and event.is_action_pressed("debug_generate_chunk"):
-		pass
-		#var new_chunk = Chunk.new()
-		#new_chunk.generate()
-		#new_chunk.position += Vector3(64, 0, 0) * len(chunks)
-		#chunks.append(new_chunk)
-		#world.add_child(new_chunk)
 		
 
 func _process(delta: float) -> void:
@@ -49,37 +43,53 @@ func _process(delta: float) -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += GRAVITY * delta
+	# Creative movement
+	if creative_mode:
+		var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if Input.is_action_pressed("move_jump"):
+			direction += Vector3(0,1,0)
+			direction = direction.normalized()
+		if Input.is_action_pressed("move_sneak"):
+			direction += Vector3(0,-1,0)
+			direction = direction.normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.y = direction.y * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = 0
+			velocity.y = 0
+			velocity.z = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		move_and_slide()
 	else:
-		velocity.x = 0
-		velocity.z = 0
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += GRAVITY * delta
+
+		# Handle jump.
+		if Input.is_action_just_pressed("move_jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = 0
+			velocity.z = 0
 
 	move_and_slide()
 	
 	
 	if looking_at:
 		if raycast.is_colliding():
-			var pos = raycast.get_collision_point() - raycast.get_collision_normal()
+			var pos = raycast.get_collision_point() - raycast.get_collision_normal() * 0.5
 			looking_at.text = "Looking at X: %f, Y: %f, Z: %f" % [pos.x, pos.y, pos.z]
-			
-	if Input.is_action_just_pressed("left_click"):
-		if raycast.is_colliding():
-			if raycast.get_collider().has_method("destroy_block"):
-				raycast.get_collider().destroy_block(raycast.get_collision_point() - raycast.get_collision_normal())
 	
 
 func get_current_chunk() -> Vector3:
